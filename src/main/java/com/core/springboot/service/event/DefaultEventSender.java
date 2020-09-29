dfe0ef47c716161eab143402c69b2fc33c53bfb8
Montaser.Sobaih <montaser.jjs@gmail.com>
2020-09-29 12:12:50 +0300

    Remove spring kafka dependency. Handle DefaultEventSender exceptions.

--- src/main/java/com/core/springboot/service/event/DefaultEventSender.java
+++ src/main/java/com/core/springboot/service/event/DefaultEventSender.java
@@ -3,6 +3,7 @@ package com.core.springboot.service.event;
 import com.core.springboot.channel.KafkaChannel;
 import com.core.springboot.model.event.Event;
 import com.core.springboot.utils.MessageBuilderFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -14,6 +15,7 @@ import org.springframework.messaging.Message;
  * @Date 29-09-2020
  */
 
+@Slf4j
 public class DefaultEventSender implements EventSender, ApplicationContextAware {
 
     private ApplicationContext context;
@@ -26,6 +28,12 @@ public class DefaultEventSender implements EventSender, ApplicationContextAware 
     @Override
     public <T extends Event<T>> boolean send(T event) {
         Message<T> message = MessageBuilderFactory.getMessageBuilder(event).build();
-        return context.getBean(event.getChannelName(), KafkaChannel.class).messageChannel().send(message);
+
+        try {
+            return context.getBean(event.getChannelName(), KafkaChannel.class).messageChannel().send(message);
+        } catch (Exception ex) {
+            log.error("No event channel found to send data");
+            return false;
+        }
     }
 }
